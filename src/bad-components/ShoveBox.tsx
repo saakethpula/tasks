31b1bce2421b752f9760e502dc38cab6d73d5204
Saaketh <saakethp@udel.edu>
2024-03-05 14:33:31 -0500

    fixed to include props

--- src/bad-components/ShoveBox.tsx
+++ src/bad-components/ShoveBox.tsx
@@ -1,38 +1,39 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
+interface ShoveBoxButtonProps {
+    position: number;
+    setPosition: (newPosition: number) => void;
+}
+
+function ShoveBoxButton({
+    position,
+    setPosition
+}: ShoveBoxButtonProps): JSX.Element {
+    return (
+        <Button onClick={() => setPosition(4 + position)}>Shove the Box</Button>
+    );
+}
+
+function MoveableBox({ position }: ShoveBoxButtonProps): JSX.Element {
+    return (
+        <div
+            data-testid="moveable-box"
+            style={{
+                width: "50px",
+                height: "50px",
+                backgroundColor: "lightblue",
+                border: "1px solid blue",
+                display: "inline-block",
+                verticalAlign: "bottom",
+                marginLeft: position + "px"
+            }}
+        ></div>
+    );
+}
+
 export function ShoveBox(): JSX.Element {
     const [position, setPosition] = useState<number>(10);
-    const box = MoveableBox();
-    function ShoveBoxButton({
-        position,
-        setPosition
-    }: {
-        position: number;
-        setPosition: (newPosition: number) => void;
-    }) {
-        return (
-            <Button onClick={() => setPosition(4 + position)}>
-                Shove the Box
-            </Button>
-        );
-    }
-
-    function MoveableBox(): JSX.Element {
-        return (
-            <div
-                data-testid="moveable-box"
-                style={{
-                    width: "50px",
-                    height: "50px",
-                    backgroundColor: "lightblue",
-                    border: "1px solid blue",
-                    display: "inline-block",
-                    verticalAlign: "bottom",
-                    marginLeft: position + "px"
-                }}
-            ></div>
-        );
-    }
+    const box = MoveableBox({ position, setPosition });
 
     return (
         <div>
