44dea185920d969790f22108a4aa9e861b07a863
Saaketh <saakethp@udel.edu>
2024-03-05 19:13:50 -0500

    finished first two

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,34 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [userInput, setUserInput] = useState("");
+    function updateInput(event: React.ChangeEvent<HTMLTextAreaElement>) {
+        setUserInput(event.target.value);
+    }
+    function checkAnswer() {
+        if (userInput === expectedAnswer) {
+            return "✔️";
+        } else {
+            return "❌";
+        }
+    }
     return (
         <div>
-            <h3>Check Answer</h3>
+            <Form.Group controlId="formMovieName">
+                <Form.Label>Input Answer:</Form.Label>
+                <Form.Control
+                    as="textarea"
+                    rows={3}
+                    value={userInput}
+                    onChange={updateInput}
+                />
+            </Form.Group>
+            <div>Your answer is {checkAnswer()}.</div>
         </div>
     );
 }
